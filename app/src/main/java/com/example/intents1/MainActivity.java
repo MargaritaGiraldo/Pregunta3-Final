b428bc3ca9afa5c7fd82b22956ff8a76c3f4788e
MargaritaGiraldo <91003379+MargaritaGiraldo@users.noreply.github.com>
2021-12-10 22:13:30 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/intents1/MainActivity.java
+++ app/src/main/java/com/example/intents1/MainActivity.java
@@ -1,8 +1,9 @@
-package com.example.intents1;
+package com.example.pregunta3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,15 +13,22 @@ public class MainActivity extends AppCompatActivity {
     //declarar paso
     Button boton;
   String valor = "Texto de prueba";
+    FirebaseAuth autentificacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // inicializamos firebase
+        autentificacion = FirebaseAuth.getInstance();
+
 // casteo
        boton= findViewById(R.id.btn_enviar);
+
 //traer al evento
        boton.setOnClickListener(new View.OnClickListener() {
+
+
            @Override
            public void onClick(View view) {
 // intenst explicito
@@ -30,5 +38,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent_explicito);
            }
        });
+
+        @Override
+    public void onClick(View view) {
+        //instent implicito
+        Intent intent_implicito = new Intent(Intent.ACTION_VIEW);
+        Intent_implicito.setData(Uri.parse("https://www.youtube.com/watch?v=KYPc7CAYJOw"));
+        startActivity(intent_implicito);
     }
-}
+});
+        boton.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View view) {
+        //instent implicito
+        string telefono = "943809532";
+        startActivity(new Intent(Intent.ACTION_CALL,Uri.parse("tel:"+telefono)));
+        }
+        });
+
+
+    public void LimpiarCajasDeTextos(){
+
+        valor.setText("");
+
+    }
